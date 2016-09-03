3c1768ee3982ee5cfb486b7fc88e06a429cf5801
Daisuke Maki <lestrrat+github@gmail.com>
2016-09-03 20:45:31 +0900

    golint

--- primitives.go
+++ primitives.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 )
 
+// UnmarshalJSON initializes the primitive type from
+// a JSON string.
 func (t *PrimitiveType) UnmarshalJSON(data []byte) error {
 	var s string
 	if err := json.Unmarshal(data, &s); err != nil {
@@ -40,6 +42,7 @@ func primitiveFromString(s string) (t PrimitiveType, err error) {
 	return
 }
 
+// String returns the string representation of this primitive type
 func (t PrimitiveType) String() string {
 	var v string
 	switch t {
@@ -63,6 +66,7 @@ func (t PrimitiveType) String() string {
 	return v
 }
 
+// MarshalJSON seriealises the primitive type into a JSON string
 func (t PrimitiveType) MarshalJSON() ([]byte, error) {
 	switch t {
 	case NullType, IntegerType, StringType, ObjectType, ArrayType, BooleanType, NumberType:
@@ -72,14 +76,15 @@ func (t PrimitiveType) MarshalJSON() ([]byte, error) {
 	}
 }
 
-func (ts *PrimitiveTypes) UnmarshalJSON(data []byte) error {
+// UnmarshalJSON initializes the list of primitive types
+func (pt *PrimitiveTypes) UnmarshalJSON(data []byte) error {
 	if data[0] != '[' {
 		var t PrimitiveType
 		if err := json.Unmarshal(data, &t); err != nil {
 			return err
 		}
 
-		*ts = PrimitiveTypes{t}
+		*pt = PrimitiveTypes{t}
 		return nil
 	}
 
@@ -88,10 +93,12 @@ func (ts *PrimitiveTypes) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	*ts = PrimitiveTypes(list)
+	*pt = PrimitiveTypes(list)
 	return nil
 }
 
+// Bool returns the underlying boolean value for the
+// primitive boolean type
 func (b Bool) Bool() bool {
 	if b.Initialized {
 		return b.Val
@@ -99,8 +106,10 @@ func (b Bool) Bool() bool {
 	return b.Default
 }
 
-func (ts PrimitiveTypes) Contains(p PrimitiveType) bool {
-	for _, v := range ts {
+// Contains returns true if the list of primitive types
+// contains `p`
+func (pt PrimitiveTypes) Contains(p PrimitiveType) bool {
+	for _, v := range pt {
 		if p == v {
 			return true
 		}
@@ -108,14 +117,18 @@ func (ts PrimitiveTypes) Contains(p PrimitiveType) bool {
 	return false
 }
 
+// Len returns the length of the list of primitive types
 func (pt PrimitiveTypes) Len() int {
 	return len(pt)
 }
 
+// Less returns true if the i-th element in the list is
+// listed before the j-th element.
 func (pt PrimitiveTypes) Less(i, j int) bool {
 	return pt[i] < pt[j]
 }
 
+// Swap swaps the elements in positions i and j
 func (pt PrimitiveTypes) Swap(i, j int) {
 	pt[i], pt[j] = pt[j], pt[i]
 }
