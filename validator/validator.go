e7b7e84b749c82eb3d12bea21c899da2101fcb7e
Daisuke Maki <lestrrat+github@gmail.com>
2016-09-03 20:38:27 +0900

    golint

--- validator/validator.go
+++ validator/validator.go
@@ -8,12 +8,15 @@ import (
 	"github.com/lestrrat/go-jsval/builder"
 )
 
+// Validator is an object that wraps jsval.Validator, and
+// can be used to validate an object against a schema
 type Validator struct {
 	lock   sync.Mutex
 	schema *schema.Schema
 	jsval  *jsval.JSVal
 }
 
+// New creates a new Validator from a JSON Schema
 func New(s *schema.Schema) *Validator {
 	return &Validator{
 		schema: s,
@@ -35,6 +38,8 @@ func (v *Validator) validator() (*jsval.JSVal, error) {
 	return v.jsval, nil
 }
 
+// Validate takes an arbitrary piece of data and
+// validates it against the schema.
 func (v *Validator) Validate(x interface{}) error {
 	jsv, err := v.validator()
 	if err != nil {
